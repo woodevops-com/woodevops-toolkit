07b7d99a90ef8f6097ed3bdc0a3461dbaa9ff5ba
Lívia Orsós <livia.orsos@codesharp.hu>
2024-03-28 15:22:11 +0100

    fix

--- package/scripts/create_nginx_config_files.php
+++ package/scripts/create_nginx_config_files.php
@@ -1,73 +1,72 @@
 <?php
 $directory = '/etc/woodevops-toolkit/pages';
-
 if (is_dir($directory)) {
-  $files = scandir($directory);
-  var_dump($file);
+  $files = array_diff(scandir($directory), array('..', '.'));
   foreach ($files as $file) {
-    var_dump($file);
-    /*$page = json_decode(file_get_contents(file), true);
-    $filename = '/etc/nginx/sites-available/'.$page['filename'];
-    $content = 'server {
-      listen 80;
-      listen [::]:80;
-      
-      root '. $page['root'] .';
+    if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+      $page = json_decode(file_get_contents(file), true);
+      $filename = '/etc/nginx/sites-available/'.$page['filename'];
+      $content = 'server {
+        listen 80;
+        listen [::]:80;
+        
+        root '. $page['root'] .';
 
-      index index.php;
+        index index.php;
 
-      server_name '. $page['domain'] .';
+        server_name '. $page['domain'] .';
 
-      client_max_body_size 100M;
+        client_max_body_size 100M;
 
-      location ~* \.(gif|jpg|png|webp|svg|css|js|ttf)$ {
-              add_header Cache-Control "public, max-age=31536000";
-              try_files $uri $uri/ =404;
+        location ~* \.(gif|jpg|png|webp|svg|css|js|ttf)$ {
+                add_header Cache-Control "public, max-age=31536000";
+                try_files $uri $uri/ =404;
+        }
+
+        location ~* "/wp-admin/admin-ajax.php" {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                fastcgi_read_timeout 300s;
+        }
+
+        location = /wp-comments-post.php {
+                deny all;
+        }
+
+        location = /xmlrpc.php {
+                deny all;
+        }
+
+        location = /wp-config.php {
+                deny all;
+        }
+
+        location = /wp-config-sample.php {
+                deny all;
+        }
+
+        location = /wp-cron.php {
+                deny all;
+        }
+
+        location / {
+                try_files $uri $uri/ /index.php?$args;
+        }
+
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                fastcgi_read_timeout 300s;
+        }
+      }';
+
+      if (file_put_contents($filename, $content) !== false) {
+          echo 'File created: ' . $page['filename'] . "\n";
+      } else {
+          echo 'Error creating: ' . $page['filename'] . "\n";
       }
-
-      location ~* "/wp-admin/admin-ajax.php" {
-              include snippets/fastcgi-php.conf;
-              fastcgi_pass unix:/run/php/php8.1-fpm.sock;
-              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-              fastcgi_read_timeout 300s;
-      }
-
-      location = /wp-comments-post.php {
-              deny all;
-      }
-
-      location = /xmlrpc.php {
-              deny all;
-      }
-
-      location = /wp-config.php {
-              deny all;
-      }
-
-      location = /wp-config-sample.php {
-              deny all;
-      }
-
-      location = /wp-cron.php {
-              deny all;
-      }
-
-      location / {
-              try_files $uri $uri/ /index.php?$args;
-      }
-
-      location ~ \.php$ {
-              include snippets/fastcgi-php.conf;
-              fastcgi_pass unix:/run/php/php8.1-fpm.sock;
-              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-              fastcgi_read_timeout 300s;
-      }
-    }';
-
-    if (file_put_contents($filename, $content) !== false) {
-        echo 'File created: ' . $page['filename'] . "\n";
-    } else {
-        echo 'Error creating: ' . $page['filename'] . "\n";
-    }*/
+    }
   }
 }
