5dbfb9bd63000c7fc553ac2630af93ebdf501bff
Lívia Orsós <livia.orsos@codesharp.hu>
2024-04-08 16:41:59 +0200

    add param to create nginx conf files

--- package/etc/woodevops-toolkit/scripts/create_nginx_config_files.php
+++ package/etc/woodevops-toolkit/scripts/create_nginx_config_files.php
@@ -1,96 +1,105 @@
 <?php
-$directory = '/etc/woodevops-toolkit/pages';
-if (is_dir($directory)) {
-  $files = array_diff(scandir($directory), array('..', '.'));
-  foreach ($files as $file) {
-    if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
-      $page = json_decode(file_get_contents($directory. '/' .$file), true);
-      $domain = implode(', ', $page['domain']);
-      echo 'sudo rm /etc/nginx/sites-enabled/'.$page['filename'];
-      $filename = '/etc/nginx/sites-available/'.$page['filename'];
-      $block_comments = 'deny all';
-      $block_xmlrpc = 'deny all';
-      if ($page['block-comments']) $block_comments = 'allow all';
-      if ($page['block-xmlrpc']) $block_xmlrpc = 'allow all';
-      $cache = '';
-      foreach ($page['cache'] as $idx => $line) {
-        if ($idx === 0) {
-          $cache .= "$line\n";
-        } else {
-          $cache .= "$line\n";
-        }
-      }
-      
-      $content = '
-      server {
-        listen 80;
-        listen [::]:80;
-        
-        root '. $page['root'] .';
 
-        index index.php;
+$arg = $argv[1];
 
-        server_name '. $domain .';
-
-        client_max_body_size 100M;
-
-        access_log /var/log/nginx/'. $page['filename'] .'_access.log;
-        error_log /var/log/nginx/'. $page['filename'] .'_error.log;
-
-        location ~* \.(gif|jpg|png|webp|svg|css|js|ttf)$ {
-                add_header Cache-Control "public, max-age=31536000";
-                try_files $uri $uri/ =404;
-        }
-
-        location ~* "/wp-admin/admin-ajax.php" {
-                include snippets/fastcgi-php.conf;
-                fastcgi_pass unix:/run/php/php8.1-fpm.sock;
-                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                fastcgi_read_timeout 300s;
-        }
-
-        location = /wp-comments-post.php {
-                '. $block_comments .';
-        } 
-
-        location = /xmlrpc.php {
-                '. $block_xmlrpc .';
-        }
-
-        location = /wp-config.php {
-                deny all;
-        }
-
-        location = /wp-config-sample.php {
-                deny all;
-        }
-
-        location = /wp-cron.php {
-                deny all;
-        }
-
-        location / {
-                try_files $uri $uri/ /index.php?$args;
-        }
-
-        location ~ \.php$ {
-                include snippets/fastcgi-php.conf;
-                fastcgi_pass unix:/run/php/php8.1-fpm.sock;
-                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                fastcgi_read_timeout 300s;
-        }
-      }';
-
-      if (file_put_contents($filename, $content) !== false) {
-          echo 'File created: ' . $page['filename'] . "\n";
-      } else {
-          echo 'Error creating: ' . $page['filename'] . "\n";
-      }
-    }
-  }
-  /*$certbot_command = "sudo certbot --nginx -n -d $domain --agree-tos --email gabor.angyal@codesharp.dev";
-  echo "Executing Certbot command: $certbot_command\n";
-  $output = shell_exec($certbot_command);
-  echo "Certbot output: $output\n";*/
+if (empty($arg)) {
+  echo 'No files provided' . "\n";
+  exit;
 }
 
+if ($arg == 'all') {
+  $files = array_diff(scandir('/etc/woodevops-toolkit/pages/'), array('..', '.'));
+  foreach ($files as $file) {
+    createNginxFile($file);
+  }
+} else {
+
+}
+
+function createNginxFile($file) {
+  if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+    $page = json_decode(file_get_contents('/etc/woodevops-toolkit/pages/' .$file), true);
+    $domain = implode(', ', $page['domain']);
+    $filename = $page['filename'];
+    echo 'sudo rm /etc/nginx/sites-enabled/'.$filename;
+    $filePath = '/etc/nginx/sites-available/'.$filename;
+    $block_comments = 'deny all';
+    $block_xmlrpc = 'deny all';
+    if ($page['block-comments']) $block_comments = 'allow all';
+    if ($page['block-xmlrpc']) $block_xmlrpc = 'allow all';
+    $cache = '';
+    foreach ($page['cache'] as $idx => $line) {
+      if ($idx === 0) {
+        $cache .= "$line\n";
+      } else {
+        $cache .= "$line\n";
+      }
+    }
+    
+    $content = '
+    server {
+      listen 80;
+      listen [::]:80;
+      
+      root '. $page['root'] .';
+
+      index index.php;
+
+      server_name '. $domain .';
+
+      client_max_body_size 100M;
+
+      access_log /var/log/nginx/'. $filename .'_access.log;
+      error_log /var/log/nginx/'. $filename .'_error.log;
+
+      location ~* \.(gif|jpg|png|webp|svg|css|js|ttf)$ {
+              add_header Cache-Control "public, max-age=31536000";
+              try_files $uri $uri/ =404;
+      }
+
+      location ~* "/wp-admin/admin-ajax.php" {
+              include snippets/fastcgi-php.conf;
+              fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+              fastcgi_param SCRIPT_filename $document_root$fastcgi_script_name;
+              fastcgi_read_timeout 300s;
+      }
+
+      location = /wp-comments-post.php {
+              '. $block_comments .';
+      } 
+
+      location = /xmlrpc.php {
+              '. $block_xmlrpc .';
+      }
+
+      location = /wp-config.php {
+              deny all;
+      }
+
+      location = /wp-config-sample.php {
+              deny all;
+      }
+
+      location = /wp-cron.php {
+              deny all;
+      }
+
+      location / {
+              try_files $uri $uri/ /index.php?$args;
+      }
+
+      location ~ \.php$ {
+              include snippets/fastcgi-php.conf;
+              fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+              fastcgi_param SCRIPT_filename $document_root$fastcgi_script_name;
+              fastcgi_read_timeout 300s;
+      }
+    }';
+
+    if (file_put_contents($filePath, $content) !== false) {
+        echo 'File created: ' . $filename . "\n";
+    } else {
+        echo 'Error creating: ' . $filename . "\n";
+    }
+  }
+}
